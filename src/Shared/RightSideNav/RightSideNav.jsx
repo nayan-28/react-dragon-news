d3faf351064da5b5130a1f62204984aebf946a12
nayan-28 <nayanmalakar720@gmail.com>
2024-02-19 00:43:54 +0600

    rightsidenav added

--- src/Shared/RightSideNav/RightSideNav.jsx
+++ src/Shared/RightSideNav/RightSideNav.jsx
@@ -6,6 +6,10 @@ import {
   FaInstagram,
 } from "react-icons/fa";
 
+import qZone1 from "../../assets/qZone1.png";
+import qZone2 from "../../assets/qZone2.png";
+import qZone3 from "../../assets/qZone3.png";
+
 const RightSideNav = () => {
   return (
     <div>
@@ -43,6 +47,12 @@ const RightSideNav = () => {
           <FaInstagram className="mr-3"></FaInstagram>
           Instagram
         </a>
+      </div>
+      <div className="p-4 space-y-2 mb-6">
+        <h2 className="text-2xl">Q Zone</h2>
+        <img src={qZone1} alt="" />
+        <img src={qZone2} alt="" />
+        <img src={qZone3} alt="" />
       </div>
     </div>
   );
