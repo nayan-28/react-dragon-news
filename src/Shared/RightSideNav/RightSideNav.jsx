f246cf96234b29c5ebd2007d954a294ae2eea1b3
nayan-28 <nayanmalakar720@gmail.com>
2024-02-19 00:37:47 +0600

    rightsidenav added

--- src/Shared/RightSideNav/RightSideNav.jsx
+++ src/Shared/RightSideNav/RightSideNav.jsx
@@ -1,7 +1,49 @@
+import {
+  FaGoogle,
+  FaGithub,
+  FaFacebook,
+  FaTwitter,
+  FaInstagram,
+} from "react-icons/fa";
+
 const RightSideNav = () => {
   return (
     <div>
-      <h2 className="text-2xl">Right Side</h2>
+      <div className="p-4 space-y-2 mb-6">
+        <h2 className="text-2xl">Login with</h2>
+        <button className="btn btn-outline w-full">
+          <FaGoogle></FaGoogle>
+          Google
+        </button>
+        <button className="btn btn-outline w-full">
+          <FaGithub></FaGithub>
+          GitHub
+        </button>
+      </div>
+      <div className="p-4 mb-6">
+        <h2 className="text-2xl mb-4">Find Us</h2>
+        <a
+          href=""
+          className="p-4 flex text-lg items-center border rounded-t-lg"
+        >
+          <FaFacebook className="mr-3"></FaFacebook>
+          Facebook
+        </a>
+        <a
+          href=""
+          className="p-4 flex text-lg items-center border rounded-t-lg"
+        >
+          <FaTwitter className="mr-3"></FaTwitter>
+          Twitter
+        </a>
+        <a
+          href=""
+          className="p-4 flex text-lg items-center border rounded-t-lg"
+        >
+          <FaInstagram className="mr-3"></FaInstagram>
+          Instagram
+        </a>
+      </div>
     </div>
   );
 };
