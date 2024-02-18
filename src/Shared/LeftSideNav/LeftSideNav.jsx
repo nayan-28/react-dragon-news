1aa30713b7c93ad4e6450dd8928594491f537d2c
nayan-28 <nayanmalakar720@gmail.com>
2024-02-19 00:53:39 +0600

    rightsidenav added

--- src/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+
 const LeftSideNav = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("categories.json")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  });
   return (
-    <div>
-      <h2 className="text-3xl">Left Side</h2>
+    <div className="space-y-6">
+      <h2 className="text-3xl">All Categories</h2>
+      {categories.map((category) => (
+        <NavLink
+          to={`/category/${category.id}`}
+          className="block ml-4 text-xl font-semibold"
+          key={category.id}
+        >
+          {category.name}
+        </NavLink>
+      ))}
     </div>
   );
 };
