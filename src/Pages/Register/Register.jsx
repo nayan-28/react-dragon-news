679004157e3424d09c14a73d618696c697d9a192
nayan-28 <nayanmalakar720@gmail.com>
2024-02-20 22:24:11 +0600

    register route added

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,8 +1,7 @@
 import { Link } from "react-router-dom";
 import Navbar from "../../Shared/Navbar/Navbar";
-
-const Login = () => {
-  const handleLogin = (e) => {
+const Register = () => {
+  const handleRegister = (e) => {
     e.preventDefault();
     const form = new FormData(e.currentTarget);
     console.log(e.currentTarget);
@@ -12,8 +11,32 @@ const Login = () => {
     <div>
       <Navbar></Navbar>
       <div>
-        <h2 className="text-3xl my-10 text-center">Login</h2>
-        <form onSubmit={handleLogin} className="md:w-3/4 lg:w-1/2 mx-auto">
+        <h2 className="text-3xl my-10 text-center">Register Here</h2>
+        <form onSubmit={handleRegister} className="md:w-3/4 lg:w-1/2 mx-auto">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Your Name"
+              name="name"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Photo URL</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Your Photo URL"
+              name="photo"
+              className="input input-bordered"
+              required
+            />
+          </div>
           <div className="form-control">
             <label className="label">
               <span className="label-text">Email</span>
@@ -37,20 +60,15 @@ const Login = () => {
               className="input input-bordered"
               required
             />
-            <label className="label">
-              <a href="#" className="label-text-alt link link-hover">
-                Forgot password?
-              </a>
-            </label>
           </div>
           <div className="form-control mt-6">
-            <button className="btn btn-primary">Login</button>
+            <button className="btn btn-success ">Register</button>
           </div>
         </form>
         <p className="my-6 text-center">
-          Do not have an account?
-          <Link to="/register" className="text-blue-600 fnt-bold">
-            Please Register
+          Already have an account ?
+          <Link to="/login" className="text-blue-600 fnt-bold">
+            Please Login
           </Link>
         </p>
       </div>
@@ -58,4 +76,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Register;
